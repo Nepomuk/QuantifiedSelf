60cb0ce0a3f83362120acfd13e424e4d3abb7a18
André Goerres <andre.goerres+git@gmail.com>
2014-12-28 12:05:24 +0100

    Create an install script to simplify the installation procedure.

---
+++ Wordcount/install_wordcount.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+PY_WORDCOUNT="wordcountDB.py"
+
+LAUNCHCTL_AGENTS="$HOME/Library/LaunchAgents"
+LAUNCHCTL_PLIST="com.nepomuk.wordcount.plist"
+
+# create the database
+/usr/bin/python $PY_WORDCOUNT init
+echo "Create database:      /usr/bin/python $PY_WORDCOUNT init"
+
+# insert the launch file into the proper folder
+sed -e "s,\${PROG_PATH},$PWD," $LAUNCHCTL_PLIST > $LAUNCHCTL_AGENTS/$LAUNCHCTL_PLIST
+echo "Copy launch script and replace path."
+
+# load launch script during runtime
+launchctl load $LAUNCHCTL_AGENTS/$LAUNCHCTL_PLIST
+echo "Load launch script:   launchctl load $LAUNCHCTL_AGENTS/$LAUNCHCTL_PLIST"
+
+echo "Installation finished."
--- Wordcount/wordcount.sh
+++ Wordcount/wordcount.sh
@@ -10,7 +10,7 @@ WC_COUNT=-1
 if [ -e $WC_BIN ]
 then
     WC_COUNT=$($WC_BIN -w $FILES | grep 'total' | awk '{count = $1} END{print count}')
-    echo "WordCount: $WC_COUNT"
+    # echo "WordCount: $WC_COUNT"
 fi
 
 # count with perl script
@@ -19,7 +19,7 @@ PC_COUNT=-1
 if [ -e $PC_BIN ]
 then
     PC_COUNT=$(perl $PC_BIN $FILES | grep 'total' | awk '{count = $1} END{print count}')
-    echo "PerlCount: $PC_COUNT"
+    # echo "PerlCount: $PC_COUNT"
 fi
 
 # count with texcount
@@ -28,7 +28,7 @@ TC_COUNT=-1
 if [ -e $TC_BIN ]
 then
     TC_COUNT=$($TC_BIN -quiet -brief -total -sum $FILES)
-    echo "TexCount:  $TC_COUNT"
+    # echo "TexCount:  $TC_COUNT"
 fi
 
 # call the python script to insert values into the database
