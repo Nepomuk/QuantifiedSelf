98682264ac83c29f2817f8e630c9db8cc45e57c9
André Goerres <andre.goerres+git@gmail.com>
2014-12-28 18:39:05 +0100

    The launchctl service is not creating the log path properly, so we take care of that.

--- Wordcount/install_wordcount.sh
+++ Wordcount/install_wordcount.sh
@@ -4,6 +4,7 @@ PY_WORDCOUNT="wordcountDB.py"
 
 LAUNCHCTL_AGENTS="$HOME/Library/LaunchAgents"
 LAUNCHCTL_PLIST="com.nepomuk.wordcount.plist"
+LAUNCHCTL_LOGDIR="/tmp/com.nepomuk.quantifiedself"
 
 # create the database
 /usr/bin/python $PY_WORDCOUNT init
@@ -15,8 +16,16 @@ echo "Get latexcount.pl:"
 wget http://ctan.mackichan.com/support/latexcount/latexcount.pl
 
 # insert the launch file into the proper folder
-sed -e "s,\${PROG_PATH},$PWD," $LAUNCHCTL_PLIST > $LAUNCHCTL_AGENTS/$LAUNCHCTL_PLIST
+sed -e "s,\${PROG_PATH},$PWD," -e "s,\${LOG_PATH},$LAUNCHCTL_LOGDIR," $LAUNCHCTL_PLIST > $LAUNCHCTL_AGENTS/$LAUNCHCTL_PLIST
 echo "Copy launch script and replace path."
+
+# unfortunately, launchctl creates the logdir with the wrong
+# permissions, so we have to take care about that
+if [ ! -d $LAUNCHCTL_LOGDIR ]
+then
+    mkdir -m 700 $LAUNCHCTL_LOGDIR
+    echo "Launchctl log path created."
+fi
 
 # load launch script during runtime
 launchctl load $LAUNCHCTL_AGENTS/$LAUNCHCTL_PLIST
